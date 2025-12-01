4df9fcc3d2187962cebccf1716c4df992d956d3b
Leonie Schweiger <leonie.schweiger@pik-potsdam.de>
2025-12-01 17:29:18 +0100

    add dimension note to common functions

--- R/calcCoGDP1900To2150.R
+++ R/calcCoGDP1900To2150.R
@@ -45,7 +45,7 @@ calcCoGDP1900To2150 <- function(scenario = "SSP2", perCapita = FALSE) {
   if (perCapita) {
     gdp <- gdp / gdpData$pop
     unit <- paste0(unit, " per capita")
-    description <- "GDP per capita from 1900-2150 yearly for the SIMSON format"
+    description <- "GDP per capita from 1900-2150 yearly"
     weight <- gdpData$pop
   }
 
@@ -53,7 +53,8 @@ calcCoGDP1900To2150 <- function(scenario = "SSP2", perCapita = FALSE) {
     x = gdp,
     weight = weight, # TODO adapt weight for per capita data
     unit = unit,
-    description = description
+    description = description,
+    note = "dimensions: (Time,Region,value)"
   )
 
 
