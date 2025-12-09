5516f7c146bcd90d07194a34cf0bfb34d82321f8
Bennet Weiss <bennet.weiss@pik-potsdam.de>
2025-12-09 17:00:11 +0100

    adjust CeProductMaterialSplit: add note and change weight.

--- R/calcCeProductMaterialSplit.R
+++ R/calcCeProductMaterialSplit.R
@@ -4,14 +4,20 @@
 calcCeProductMaterialSplit <- function(){
   x <- readSource("Cao2024", subtype = "product_material_split")
 
-  # create new magpie object and fill with ones
-  weight <- new.magpie(cells_and_regions = NULL)
-  weight <- toolCountryFill(weight, fill = 1, verbosity = 2)
+  weight <- toolCumulativeCementProduction(x)
 
   unit <- "ratio"
   description <- paste(
     "Split cement into product materials.",
     "Data from Cao2024."
   )
-  output <- list(x = x, weight = weight, unit = unit, description = description)
+  note = "dimensions: (Region,Product Material,value)"
+
+  output <- list(
+    x = x,
+    weight = weight,
+    unit = unit,
+    description = description,
+    note = note
+  )
 }
