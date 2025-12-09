378d802c9198bcf19f5c27362b3398f5bd1cdfb3
Bennet Weiss <bennet.weiss@pik-potsdam.de>
2025-12-09 16:59:22 +0100

    split nonres lifetime into com and ind

--- R/calcCeBuiltLifespan.R
+++ R/calcCeBuiltLifespan.R
@@ -2,6 +2,14 @@
 #' @author Bennet Weiss
 calcCeBuiltLifespan <- function() {
   data <- readSource("PostedBuiltLifespan")
+
+  # Split NonRes into Com and Ind and remove NonRes category
+  # TODO: reflect this in POSTED instead of here
+  data_new <- add_columns(data, addnm = c("Com", "Ind"), dim = 3.1)
+  data_new[, , "Com"] <- data[, , "NonRes"]
+  data_new[, , "Ind"] <- data[, , "NonRes"]
+  data_final <- data_new[, , "NonRes", invert = TRUE]
+
   unit <- "years (a)"
   description <- paste(
     "Lifetimes of residential and non-residential buildings, as well as of civil engeneering.",
@@ -9,10 +17,10 @@ calcCeBuiltLifespan <- function() {
     "Documentation can be found in Posted (https://github.com/PhilippVerpoort/posted)"
   )
   note <- "dimensions: (Historic Time,Region,Stock Type,value)"
-  weight <- data
-  weight[, , ] <- 1
+  weight <- toolCumulativeCementProduction(data_final)
+
   output <- list(
-    x = data,
+    x = data_final,
     weight = weight,
     unit = unit,
     description = description,
