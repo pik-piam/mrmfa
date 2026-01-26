8cc17593930ad074df5a50521ef9238ee1483441
Bennet Weiss <bennet.weiss@pik-potsdam.de>
2026-01-26 19:53:49 +0100

    refactor GEM floorspace calculation following review suggestions

--- R/calcCeFloorspaceGEM.R
+++ R/calcCeFloorspaceGEM.R
@@ -1,42 +1,41 @@
 #' Calculate total floorspace of buildings from GEM, optionally grouped by subtype.
 #'
-#' @param subtype Floorspace grouped by: "Stock_Type", "Structure", "Function", NULL (i.e. all).
+#' @param subtype str or list(str) or None, Categories by which floorspace is resolved. Other dimensions are aggregated.
+#'                Possible entries: "Stock_Type", "Structure", "Function", NULL (i.e. all). Defaults to NULL.
 #' @param remove_ind Logical, if TRUE (default), industrial buildings are removed from the dataset.
 #' @author Bennet Weiss
 calcCeFloorspaceGEM <- function(subtype = NULL, remove_ind = TRUE) {
-  possible_subtypes <- c("Stock_Type", "Structure", "Function")
+  possible_subtypes <- c("Stock_Type", "Function", "Structure")
 
-  data <- readSource("GlobalExposureModel")
-
-  # transform to df for easier handling
-  df <- as.data.frame(data, rev = 3)
+  floorArea <- readSource("GlobalExposureModel")
 
   if (remove_ind) {
-    df <- df[df$Stock_Type != "Ind", ]
+    floorArea <- mselect(floorArea, "Stock_Type" = c("Res", "Com"))
+    floorArea <- mselect(floorArea, "Function" = c("Com", "RM", "RS"))
   }
-
-  # transform back to magpie
-  floorArea <- as.magpie(df, spatial = 1)
 
   if (is.null(subtype)){
     x_out <- floorArea
   } else if (all(subtype %in% possible_subtypes)) {
-    df_byType <- group_by(df, ISO3, across(all_of(subtype))) %>%
-      summarise(TOTALAREA_SQM = sum(.value, na.rm = TRUE), .groups = "drop")
-    floorArea_byType <- as.magpie(df_byType, spatial = 1)
-
-    x_out <- floorArea_byType
+    dims_to_sum <- setdiff(possible_subtypes, subtype)
+    x_out <- dimSums(floorArea, dim = dims_to_sum, na.rm = T)
   } else {
     stop(paste("Invalid subtype: must be one of ", paste(possible_subtypes, collapse = ", ")))
   }
 
+  # sort dimensions in expected order given by possible_subtypes
+  current_dim_order <- getSets(x_out, fulldim = TRUE)[-(1:2)] # exclude region and time dim
+  target_order <- match(subtype, current_dim_order)
+  x_out <- dimOrder(x_out, perm = target_order, dim = 3)
+
   # output
+  subtype <- gsub("_", " ", subtype)
   x_out[is.na(x_out)] <- 0
   weight <- NULL
   unit <- "square meter"
   description <- paste(
     "Total buildings floor area by ",
-    paste(setdiff(possible_subtypes, subtype), collapse = " and "), ".",
+    paste(subtype, collapse = " and "), ".",
     "Data from Global Exposure Model (GEM), categories harmonized with RASMI.",
     "Yepes-Estrada, C., Calderon, A., Costa, C., Crowley, H., Dabbeek, J., Hoyos, M., Martins, L., Paul, N., Rao, A., Silva, V. (2023).",
     "Global Building Exposure Model for Earthquake Risk Assessment. Earthquake Spectra. doi:10.1177/87552930231194048."
