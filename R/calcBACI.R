0daf3018183f29ccc1b373cbbe5d6c4fabaf3201
Leonie Schweiger <leonie.schweiger@pik-potsdam.de>
2026-02-12 13:06:36 +0100

    fix linter warnings

--- R/calcBACI.R
+++ R/calcBACI.R
@@ -23,14 +23,13 @@
 #' @importFrom magclass as.magpie getComment<-
 #'
 calcBACI <- function(subtype, HS = "02") {
-
   # Read raw data
   BACI_data <- readSource("BACI", subtype = subtype, subset = HS)
   df <- quitte::madrat_mule(BACI_data)
 
-  if(subtype == "plastics_UNCTAD"){
+  if (subtype == "plastics_UNCTAD") {
     final <- df
-  }else if (subtype == "plastics_UNEP"){
+  } else if (subtype == "plastics_UNEP") {
     # map UNEP-NGP sectors to sectors used in REMIND-MFA
     sector_map <- toolGetMapping("sectormappingUNEP_NGP.csv", type = "sectoral", where = "mrmfa")
     new1 <- df %>%
@@ -42,14 +41,14 @@ calcBACI <- function(subtype, HS = "02") {
     polymer_map <- toolGetMapping("polymermappingUNEP_NGP.csv", type = "sectoral", where = "mrmfa")
     # use polymer use by sector as weights (summarize over all Regions, as polymer share by sector is constant over all Regions in OECD data)
     # use total polymer use over all sectors as weights for "General" sector
-    use <- calcOutput("PlOECD",subtype = "Use_2019_region", aggregate = TRUE) %>% as.data.frame()
+    use <- calcOutput("PlOECD", subtype = "Use_2019_region", aggregate = TRUE) %>% as.data.frame()
     use_by_sector <- use %>%
       rename(sector = "Data2", polymer = "Data1") %>%
-      filter(.data$polymer!="Total") %>%
+      filter(.data$polymer != "Total") %>%
       group_by(.data$sector, .data$polymer) %>%
       summarize(value = sum(.data$Value)) %>%
-      mutate(sector = case_when(.data$sector=="Total"~"General", .default=.data$sector))
-    split <- merge(polymer_map, use_by_sector, by.y="polymer", by.x="Target") %>%
+      mutate(sector = case_when(.data$sector == "Total" ~ "General", .default = .data$sector))
+    split <- merge(polymer_map, use_by_sector, by.y = "polymer", by.x = "Target") %>%
       group_by(.data$sector, .data$Source) %>%
       mutate(
         total = sum(.data$value, na.rm = TRUE),
@@ -58,9 +57,10 @@ calcBACI <- function(subtype, HS = "02") {
       select(-"total", -"value")
     new2 <- left_join(new1, split, by = c("polymer" = "Source", "sector")) %>%
       mutate(value = .data$value * .data$weight)
-    # some weights are NaN for polymers that are not used in a specific sector according to OECD, throw a warning if these combination appear in the dataset
+    # some weights are NaN for polymers that are not used in a specific sector according to OECD,
+    # throw a warning if these combination appear in the dataset
     nan <- new2 %>% filter(is.na(.data$weight))
-    if (nrow(nan)>0){
+    if (nrow(nan) > 0) {
       warning(paste(
         "The following sector-polymer combinations cannot be mapped from the BACI data as they do not exist in the OECD dataset used for weighting:\n",
         paste(capture.output(print(nan)), collapse = "\n")
@@ -71,9 +71,8 @@ calcBACI <- function(subtype, HS = "02") {
       select(-"polymer", -"weight") %>%
       rename("polymer" = "Target") %>%
       group_by(.data$t, .data$exporter, .data$importer, .data$polymer, .data$stage, .data$sector) %>%
-      summarize(value=sum(.data$value)) %>%
+      summarize(value = sum(.data$value)) %>%
       ungroup()
-
   }
 
   x <- as.magpie(final, temporal = "t", spatial = "importer")
@@ -83,29 +82,28 @@ calcBACI <- function(subtype, HS = "02") {
   # define a custom aggregation function that filters out all intra-regional trade
   # and returns both imports and exports for each region in the region mapping
   .customAggregate <- function(x, rel) {
-
     df <- tibble::as_tibble(x)
 
     # get grouping variables
-    group_vars = setdiff(colnames(df), c("t","importer","exporter","value"))
+    group_vars <- setdiff(colnames(df), c("t", "importer", "exporter", "value"))
 
     # make sure that exporter_region != importer_region for every entry
     df <- df %>%
-      left_join(rel[,c("country", "region")], by = c("importer" = "country")) %>%
-      left_join(rel[,c("country", "region")], by = c("exporter" = "country")) %>%
+      left_join(rel[, c("country", "region")], by = c("importer" = "country")) %>%
+      left_join(rel[, c("country", "region")], by = c("exporter" = "country")) %>%
       select("t", "importer" = "region.x", "exporter" = "region.y", all_of(group_vars), "value") %>%
       filter(.data$importer != .data$exporter)
 
     imports <- df %>%
       group_by(.data$t, .data$importer, across(all_of(group_vars))) %>%
-      summarize(value = sum(.data$value, na.rm=TRUE)) %>%
+      summarize(value = sum(.data$value, na.rm = TRUE)) %>%
       ungroup() %>%
       rename("region" = "importer") %>%
       mutate("type" = "Imports")
 
     exports <- df %>%
       group_by(.data$t, .data$exporter, across(all_of(group_vars))) %>%
-      summarize(value = sum(.data$value, na.rm=TRUE)) %>%
+      summarize(value = sum(.data$value, na.rm = TRUE)) %>%
       ungroup() %>%
       rename("region" = "exporter") %>%
       mutate("type" = "Exports")
--- R/readBACI.R
+++ R/readBACI.R
@@ -22,22 +22,24 @@
 #' @importFrom dplyr select filter rename summarize ungroup
 #'
 readBACI <- function(subset = "02", subtype) {
-
   # check whether subset is one of the available HS revisions
   available <- c("92", "02", "17", "22")
   if (is_empty(intersect(subset, available))) {
     stop(
       "Invalid subset -- supported HS revisions are:",
       paste0(available, collapse = ", ")
-    )}
+    )
+  }
 
   # read HS codes that are relevant for the scope defined in subtype
   if (subtype == "plastics_UNCTAD") {
     # if an older HS revision than 2002 is used, use the oldest available (2002)
-    if(!(subset %in% c("02","07","12","17","22"))){
-      HS = "02"
-    } else{HS=subset}
-    UNCTAD_revision <- paste0("DimHS20",HS,"Products_Plastics_Hierarchy.xls")
+    if (!(subset %in% c("02", "07", "12", "17", "22"))) {
+      HS <- "02"
+    } else {
+      HS <- subset
+    }
+    UNCTAD_revision <- paste0("DimHS20", HS, "Products_Plastics_Hierarchy.xls")
     UNCTAD_product_codes <- read_excel(file.path("UNCTAD_PlasticsHSCodes", UNCTAD_revision), skip = 2)
     # Identify header rows
     is_header <- grepl("^P_", UNCTAD_product_codes[[1]])
@@ -45,33 +47,38 @@ readBACI <- function(subset = "02", subtype) {
     UNCTAD_product_codes$Group <- UNCTAD_product_codes[[2]][is_header][cumsum(is_header)]
     # Remove the header rows
     codes <- UNCTAD_product_codes[!is_header, ] %>%
-      dplyr::rename(code="Code", group="Group")
+      dplyr::rename(code = "Code", group = "Group")
     codes$code <- as.integer(codes$code)
-  } else if (subtype == "plastics_UNEP"){
-    codes <- read_excel(file.path("UNEP_NGP","TOOL_T1.4a_v1.2_Trade data modelling.xlsx"),
-                        sheet = "SelectedCOMCodes", skip = 12) %>%
-      dplyr::select("code" = "Code", "polymer" = "Polymer Type", "application" = "Application Type", "stage" ="Type", "sector" = "Sector", "label" = "Extensive description on comtrade", "plastic_percentage" = "Plastic percentage")
+  } else if (subtype == "plastics_UNEP") {
+    codes <- read_excel(file.path("UNEP_NGP", "TOOL_T1.4a_v1.2_Trade data modelling.xlsx"),
+      sheet = "SelectedCOMCodes", skip = 12
+    ) %>%
+      dplyr::select("code" = "Code", "polymer" = "Polymer Type", "application" = "Application Type",
+                    "stage" = "Type", "sector" = "Sector", "label" = "Extensive description on comtrade",
+                    "plastic_percentage" = "Plastic percentage")
     # remove duplicates in raw data; label all polymers in the textile sector as "Fibres"
     codes <- unique(codes) %>%
-      mutate(polymer = case_when(.data$sector=="Textile"~"Fibres", .default=.data$polymer))
+      mutate(polymer = case_when(.data$sector == "Textile" ~ "Fibres", .default = .data$polymer))
     # get selected 4-digit and 6-digit COMTRADE codes from UNEP_NGP
-    UNEP_codes_k4 <- codes %>% filter(.data$code<10000)
-    UNEP_codes_k6 <- codes %>% filter(.data$code>10000)
+    UNEP_codes_k4 <- codes %>% filter(.data$code < 10000)
+    UNEP_codes_k6 <- codes %>% filter(.data$code > 10000)
   } else {
     stop("Invalid subtype. Choose either 'plastics_UNCTAD' or 'plastics_UNEP'.")
   }
 
   # Read raw trade data of respective HS classification system
-  data_path = paste0("BACI_HS", subset, "_V202501")
+  data_path <- paste0("BACI_HS", subset, "_V202501")
   files <- list.files(
     data_path,
-    pattern = paste0("^BACI_HS", subset,"_Y[0-9]{4}_V[0-9]+\\.csv$"),
+    pattern = paste0("^BACI_HS", subset, "_Y[0-9]{4}_V[0-9]+\\.csv$"),
     full.names = TRUE
   )
   # Read country codes of the dataset
-  country_codes <- read.csv(file.path(data_path,"country_codes_V202501.csv")) %>% select("country_code","country_iso3") %>%
-    # country code 490 (country_iso3="S19") is used as a proxy for trade statistics for Taiwan (see https://www.cepii.fr/DATA_DOWNLOAD/baci/doc/baci_webpage.html)
-    mutate(country_iso3 = case_when(.data$country_iso3=="S19"~"TWN", .default=.data$country_iso3))
+  country_codes <- read.csv(file.path(data_path, "country_codes_V202501.csv")) %>%
+    select("country_code", "country_iso3") %>%
+    # country code 490 (country_iso3="S19") is used as a proxy for trade statistics for Taiwan
+    # (see https://www.cepii.fr/DATA_DOWNLOAD/baci/doc/baci_webpage.html)
+    mutate(country_iso3 = case_when(.data$country_iso3 == "S19" ~ "TWN", .default = .data$country_iso3))
 
   df_all <- NULL
 
@@ -81,33 +88,37 @@ readBACI <- function(subset = "02", subtype) {
     # filter HS codes that are relevant for the scope defined in subtype
     if (subtype == "plastics_UNCTAD") {
       # merge UNCTAD codes with BACI data
-      df_filtered <- merge(df, codes, by.x="k", by.y="code") %>% select("t", "i", "j", "k", "group", "q")
-    } else if (subtype == "plastics_UNEP"){
-      # UNEP Codes contain 4 digit and 5/6 digit codes; in order to merge 4 digit codes, transform 6-digit codes in BACI database to 4 digits
-      df_UNEP <- df %>% mutate(k4 = as.integer(as.integer(.data$k/100)))
-      df_plastics_k4 <- merge(UNEP_codes_k4, df_UNEP, by.y="k4", by.x="code") %>% select(-"k")
-      df_plastics_k6 <- merge(UNEP_codes_k6, df_UNEP, by.y="k", by.x="code")%>% select(-"k4")
+      df_filtered <- merge(df, codes, by.x = "k", by.y = "code") %>% select("t", "i", "j", "k", "group", "q")
+    } else if (subtype == "plastics_UNEP") {
+      # UNEP Codes contain 4 digit and 5/6 digit codes;
+      # in order to merge 4 digit codes, transform 6-digit codes in BACI database to 4 digits
+      df_UNEP <- df %>% mutate(k4 = as.integer(as.integer(.data$k / 100)))
+      df_plastics_k4 <- merge(UNEP_codes_k4, df_UNEP, by.y = "k4", by.x = "code") %>% select(-"k")
+      df_plastics_k6 <- merge(UNEP_codes_k6, df_UNEP, by.y = "k", by.x = "code") %>% select(-"k4")
       # merge filtered data with 4 and 6 digit codes, calculate plastics content
-      df_filtered <- rbind(df_plastics_k6, df_plastics_k4) %>% rename(k = "code") %>%
-        mutate(q_plastic = .data$plastic_percentage*.data$q) %>%
+      df_filtered <- rbind(df_plastics_k6, df_plastics_k4) %>%
+        rename(k = "code") %>%
+        mutate(q_plastic = .data$plastic_percentage * .data$q) %>%
         group_by(.data$t, .data$i, .data$j, .data$k, .data$polymer, .data$stage, .data$sector) %>%
-        summarize(q=sum(.data$q_plastic, na.rm=TRUE)) %>%
+        summarize(q = sum(.data$q_plastic, na.rm = TRUE)) %>%
         ungroup()
     }
 
     df_filtered <- df_filtered %>%
-      merge(country_codes, by.x="i", by.y="country_code") %>% rename(exporter = "country_iso3")%>%
-      merge(country_codes, by.x="j", by.y="country_code") %>% rename(importer = "country_iso3")%>%
-      mutate(value=.data$q/1000000) %>% #report quantity in Mt
+      merge(country_codes, by.x = "i", by.y = "country_code") %>%
+      rename(exporter = "country_iso3") %>%
+      merge(country_codes, by.x = "j", by.y = "country_code") %>%
+      rename(importer = "country_iso3") %>%
+      mutate(value = .data$q / 1000000) %>% # report quantity in Mt
       select(-"i", -"j", -"q")
 
     df_all <- rbind(df_all, df_filtered)
   }
 
   # which Codes are missing?
-  diff <- setdiff(unique(codes$code),unique(df_all$k))
-  missing <- codes %>% filter(.data$code %in%diff)
-  if (length(diff)>0){
+  diff <- setdiff(unique(codes$code), unique(df_all$k))
+  missing <- codes %>% filter(.data$code %in% diff)
+  if (length(diff) > 0) {
     warning(paste(
       "The following product codes are missing in the BACI dataset:\n",
       paste(capture.output(print(missing)), collapse = "\n")
@@ -115,19 +126,19 @@ readBACI <- function(subset = "02", subtype) {
   }
 
   # get grouping variables
-  group_vars = setdiff(colnames(df_all), c("t","k","importer","exporter","value"))
+  group_vars <- setdiff(colnames(df_all), c("t", "k", "importer", "exporter", "value"))
   # remove data that is unreasonable (extreme outliers) and interpolate instead
   unreasonable <- data.frame(
     exporter = c("MEX", "NGA", "NGA", "CHE"),
     importer = c("USA", "ATG", "ATG", "MOZ"),
-    t   = c(2004, 2010, 2011, 2016),
-    k   = c(392310, 550320, 550320, 6309),
+    t = c(2004, 2010, 2011, 2016),
+    k = c(392310, 550320, 550320, 6309),
     flag_unreasonable = TRUE
   )
   df_clean <- df_all %>%
     # remove trade data of 220190 "Waters; other than mineral and aerated, (not containing added sugar or other sweetening matter nor flavoured), ice and snow"
     # as this category unreasonably inflates trade between HKG and CHN
-    filter(.data$k!=220190) %>%
+    filter(.data$k != 220190) %>%
     left_join(
       unreasonable,
       by = c("t", "exporter", "importer", "k")
@@ -139,10 +150,9 @@ readBACI <- function(subset = "02", subtype) {
     ungroup()
 
   # retain only relevant dimensions
-  group_vars2 = setdiff(group_vars, "k")
   df_final <- df_clean %>%
     group_by(.data$t, .data$exporter, .data$importer, across(all_of(group_vars))) %>%
-    summarize(value=sum(.data$value_interp)) %>%
+    summarize(value = sum(.data$value_interp)) %>%
     ungroup()
 
   return(quitte::madrat_mule(df_final))
