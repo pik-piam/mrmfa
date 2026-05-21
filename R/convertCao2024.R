770a105d4489b1b8052939c9fe52876f81ed2851
Bennet Weiss <bennet.weiss@pik-potsdam.de>
2026-05-21 11:49:31 +0200

    move mappings from mappingfolder to library

--- R/convertCao2024.R
+++ R/convertCao2024.R
@@ -6,7 +6,7 @@ convertCao2024 <- function(x) {
   if (length(x) == 1) {
     x_out <- toolCountryFill(x, fill = x[1], verbosity = 2)
   } else {
-    region_mapping <- toolGetMapping("regionmappingR10_extended.csv", where = "mappingfolder", type = "regional")
+    region_mapping <- toolGetMapping("regionmappingR10_extended.csv", where = "mrmfa", type = "regional")
     x_out <- toolAggregate(x, region_mapping, from = "RegionName", to = "CountryCode")
   }
 
