0f5ef7f973e02ee16035f0f617199b4c682de5e8
Bennet Weiss <bennet.weiss@pik-potsdam.de>
2026-05-21 12:06:36 +0200

    address review comment:: add Kosovo to Serbia

--- R/convertGlobalExposureModel.R
+++ R/convertGlobalExposureModel.R
@@ -3,7 +3,7 @@
 #' @author Bennet Weiss
 #' @param x Magpie object
 convertGlobalExposureModel <- function(x) {
-  no_remove_warning <- "XKX" # Kosovo treated as part of Serbia, Serbia already exists.
-  x_out <- madrat::toolCountryFill(x, verbosity = 2, no_remove_warning = no_remove_warning)
+  x["SRB", ] <- x["SRB", ] + toolNAreplace(x["XKX", ])$x # add Kosovo to Serbia
+  x_out <- madrat::toolCountryFill(x, verbosity = 2)
   return(x_out)
 }
